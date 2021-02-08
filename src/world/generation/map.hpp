ec9a5f321c22fbe9d28c341fef0594eb7984538d
Duco Lindhout <duco.lindhout@gmail.com>
2021-02-08 20:21:11 +0100

    Some map generation: heat and height

--- src/world/generation/map.hpp
+++ src/world/generation/map.hpp
@@ -7,9 +7,10 @@
 // Not a chunk or a real game square 
 struct Tile {
     float height; // Z height
+    float temperature; // Lower is colder
+    float wetness;
 
-    Tile() {}
-    Tile(float _height);
+    Tile *left, *top, *right, *bottom = nullptr;
 };
 
 // The Map is what stores all biome, weather, season, temerature data
@@ -17,7 +18,8 @@ struct Tile {
 class Map {
     public:
         Map(uint32_t map_width, uint32_t map_height);
-        void initTiles();
+        void initHeightmap();
+        void initHeatmap();
 
         uint32_t width; // Size of map
         uint32_t height;
